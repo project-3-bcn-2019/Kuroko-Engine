80cfafd347078493e014612a2653daaad470b7b8
Marc Torres Jimenez <mtorres.form@gmail.com>
2019-03-03 00:10:12 +0100

    Comitting base of the Animation Event panel
    will separate the animation panel from the animationevent panel to properly target each piece of the animation system
    
    TODO:
    -Frame Remove
    -Frame Editing
    -*Clickable* Animation Timeline if possible
    
    When done merge back to master and push for the design team to use this for linking sound to animation

--- Source/PanelAnimationEvent.h
+++ Source/PanelAnimationEvent.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include "Panel.h"
+#include <list>
+
+class Component;
+class ComponentAnimation;
+class ResourceAnimation;
+class GameObject;
+
+class PanelAnimationEvent : public Panel
+{
+public:
+
+	PanelAnimationEvent(const char* name);
+
+	bool fillInfo();
+
+	~PanelAnimationEvent();
+
+	void Draw();
+	
+};
